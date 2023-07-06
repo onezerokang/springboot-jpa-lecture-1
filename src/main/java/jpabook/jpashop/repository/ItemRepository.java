69672bfe23f6870b9014645cb42ca35efe815da3
onezerokang <onezerokang@gmail.com>
2023-07-06 14:20:29 +0900

    상품 리포지토리 개발

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+    private final EntityManager em;
+
+    public void save(Item item) {
+        // item은 jpa에 저장하기 전가지 id 값이 없다.
+        // 새로 생성한 객체면 em.persist를 호출하고
+        // db에 등록된 객체라면 업데이트 한다.
+        if (item.getId() == null) {
+            em.persist(item);
+        } else {
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+}
